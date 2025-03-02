9558c13a124badb7d0e5cc6eb7b3af46a3b661cd
MdShan98 <mdshan1820@gmail.com>
2025-03-02 21:20:37 +0400

    checking eveything

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+// color changing function//
+
+const colorChange = document.getElementById('color-change');
+colorChange.addEventListener('click',function(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
+
+//changing the number//
 const mainNumber = document.getElementById('main-number').innerText;
 
-    const convertedNumber = parseFloat(mainNumber);
-    const sum = convertedNumber + 1;
+const convertedNumber = parseFloat(mainNumber);
+const sum = convertedNumber + 1;
 
     // number cutting
 
-    const taskNumber = document.getElementById('task-number').innerText;
-    const convertedNum = parseFloat(taskNumber);
-    const minus = convertedNum - 1;
+const taskNumber = document.getElementById('task-number').innerText;
+const convertedNum = parseFloat(taskNumber);
+const minus = convertedNum - 1;
 
 document.getElementById('btn-1').addEventListener('click',function(){
     alert('Board added successfully');
@@ -72,20 +81,21 @@ document.getElementById('btn-6').addEventListener('click',function(){
     newMassage.innerText = 'You have completed the task Improve Job Searching at Time: ' +  currentTime;
     massageSection.appendChild(newMassage);
 });
-
+//massage clearing function//
 document.getElementById('clear').addEventListener('click',function(){
     const passageSection = document.getElementById('massage-Section');
     passageSection.innerText = '';
 });
-
+//move to another file//
 window.onload = function() {
     const clickDiv = document.getElementById("faq");
     clickDiv.addEventListener("click", function() {
       window.location.href = "faq.html";
     });
   };
+
 // window.onload = function() {
-//     const goBack = document.getElementById("back");
+//     const goBack = document.getElementById('backk');
 //     goBack.addEventListener("click", function() {
 //       window.location.href = "index.html";
 //     });

b8013d2e1c4c122f8c388658c1fa9678150a4a7d
MdShan98 <mdshan1820@gmail.com>
2025-03-02 20:38:52 +0400

    function added to cart

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const mainNumber = document.getElementById('main-number').innerText;
+
+    const convertedNumber = parseFloat(mainNumber);
+    const sum = convertedNumber + 1;
+
+    // number cutting
+
+    const taskNumber = document.getElementById('task-number').innerText;
+    const convertedNum = parseFloat(taskNumber);
+    const minus = convertedNum - 1;
+
 document.getElementById('btn-1').addEventListener('click',function(){
     alert('Board added successfully');
     const massageSection = document.getElementById('massage-Section');
@@ -5,10 +16,16 @@ document.getElementById('btn-1').addEventListener('click',function(){
     const newMassage = document.createElement('p');
     
     newMassage.innerText = 'You have completed the task mobile fix issue at Time: ' +  currentTime;
-    newMassage.classList.add('myMsg');
+    newMassage.classList.add('myMsg');   
 
     massageSection.appendChild(newMassage);
+    // number adding//
+    document.getElementById('main-number').innerText = sum;
+
+    document.getElementById('task-number').innerText = minus;
 });
+
+
 
 document.getElementById('btn-2').addEventListener('click',function(){
     alert('Board added successfully');
@@ -17,6 +34,10 @@ document.getElementById('btn-2').addEventListener('click',function(){
     const newMassage = document.createElement('p');
     newMassage.innerText = 'You have completed the task Add New Emoji at Time: ' +  currentTime;
     massageSection.appendChild(newMassage);
+
+    document.getElementById('main-number').innerText = sum;
+
+    document.getElementById('task-number').innerText = minus;
 });
 
 document.getElementById('btn-3').addEventListener('click',function(){
@@ -52,8 +73,22 @@ document.getElementById('btn-6').addEventListener('click',function(){
     massageSection.appendChild(newMassage);
 });
 
-// document.getElementById('clear').addEventListener('click',function(){
-    
-// });
+document.getElementById('clear').addEventListener('click',function(){
+    const passageSection = document.getElementById('massage-Section');
+    passageSection.innerText = '';
+});
+
+window.onload = function() {
+    const clickDiv = document.getElementById("faq");
+    clickDiv.addEventListener("click", function() {
+      window.location.href = "faq.html";
+    });
+  };
+// window.onload = function() {
+//     const goBack = document.getElementById("back");
+//     goBack.addEventListener("click", function() {
+//       window.location.href = "index.html";
+//     });
+//   };
 
 
